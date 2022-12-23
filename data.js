9adb435e9682b60e5513496089f6eb521529a4df
Katya Panko <katena-1990@mail.ru>
2022-12-23 21:07:01 +0300

    data jc

--- data.js
+++ data.js
@@ -1,35 +1,35 @@
 const dataElement = `[{
-    "img_src": "D:\курсы\js домашка\ex11\img\product1.svg",
+    "img_src": "img/product1.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
     },
     {
-    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "img_src": "img/product2.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
     },
     {
-    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "img_src": "img/product3.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
     },
     {
-    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "img_src": "img/product4.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
     },
     {
-    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "img_src": "img/product5.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
     },
     {
-    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "img_src": "img/product6.svg",
     "subtitle": "ellery x m'o capsule",
     "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "$52.00"
