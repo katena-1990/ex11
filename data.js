fa29cee20b87ce84d764f916c56fdd86126af9c7
Katya Panko <katena-1990@mail.ru>
2022-12-23 12:01:34 +0300

    json

---
+++ data.js
@@ -0,0 +1,37 @@
+const dataElement = `[{
+    "img_src": "D:\курсы\js домашка\ex11\img\product1.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    },
+    {
+    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    },
+    {
+    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    },
+    {
+    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    },
+    {
+    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    },
+    {
+    "img_src": "D:\курсы\js домашка\ex11\img\product2.svg",
+    "subtitle": "ellery x m'o capsule",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+    }
+]`
